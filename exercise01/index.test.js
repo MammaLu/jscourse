bd83f2357b2585021051794c423b1a9b4db82f17
MammaLu <katerinashchotkina@gmail.com>
2024-09-10 18:41:13 +0200

    test passed

--- exercise01/index.test.js
+++ exercise01/index.test.js
@@ -1,6 +1,5 @@
-import {expect, describe, it} from "vitest";
-import {contains} from "exercise01/index.js"
-
+import { contains } from "../exercise01/index";
+import { describe, it, expect} from "vitest";
 describe('contains', () => {
 it('if there is the space return true', () => {
     expect(contains("Hello World", " ")).toBe(true);
